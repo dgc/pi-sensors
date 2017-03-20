cb447e0a33f53c8bac59607aa80b3a5502c881be
Don Cruickshank <dgc@soton.ac.uk>
2017-03-20 14:01:36 +0000

    Changed report health to publish directly to status channel.

--- report_health.sh
+++ report_health.sh
@@ -10,4 +10,4 @@ MAC_ADDR=`/sbin/ifconfig eth0 | /bin/grep -oP 'HWaddr [0-9a-f]{2}:[0-9a-f]{2}:[0
 SPACE=`/bin/df / | /usr/bin/awk '{if (NR!=1) {print}}' | /bin/grep -oP '[0-9]+%' | /bin/grep -oP '[0-9]+'`
 LOAD=`/usr/bin/uptime | /bin/grep -oP 'load average: [0-9.]+,' | /bin/grep -oP '[0-9.]+'`
 
-echo "{\"timestamp\":$TIMESTAMP,\"device\":\"$DEVICE\",\"ipv4_address\":\"$IPV4_ADDR\",\"mac_address\":\"$MAC_ADDR\",\"space\":$SPACE,\"load\":$LOAD}"
+/usr/bin/mosquitto_pub -h $MQTT_HOST -t $MQTT_CHAN -m "{\"timestamp\":$TIMESTAMP,\"device\":\"$DEVICE\",\"ipv4_address\":\"$IPV4_ADDR\",\"mac_address\":\"$MAC_ADDR\",\"space\":$SPACE,\"load\":$LOAD}"
